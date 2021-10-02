0ce8745383a52ed4ea17909eb0ea3019d06364a3
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-02 14:20:36 -0300

    Atividade API Rest adicionada e finalizada
    Projeto API REST adicionado e suas exigências foram finalizadas.

---
+++ Atividade API REST/Atividade API REST/Controllers/PokemonController.cs
@@ -0,0 +1,47 @@
+﻿using Atividade_API_REST.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade_API_REST.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class PokemonController : ControllerBase
+	{
+		static List<Pokemon> PokemonsCadastrados = new List<Pokemon>();
+		static int ContadorHipotetico = 0;
+
+		[HttpPost]
+		public void AdicionaPokemon([FromBody] Pokemon pokemon)
+		{
+			ContadorHipotetico++;
+			pokemon.Codigo = ContadorHipotetico;					
+			PokemonsCadastrados.Add(pokemon);
+			Console.WriteLine("Pokemon Novo Adicionado");
+		}
+
+		[HttpGet]
+		public IEnumerable<Pokemon> RecuperaTodosOsPokemons()
+		{
+			Console.WriteLine("Retornando Todos os Pokemons");
+			return PokemonsCadastrados;
+		}
+
+		[HttpGet("{id}")]
+		public Pokemon RecuperaPokemonPorId(int Id)
+		{
+			Console.WriteLine("Retornando Pokemon de Id {0}", Id);
+			return PokemonsCadastrados.Find(P => P.Codigo == Id);
+		}
+
+		[HttpDelete("{id}")]
+		public void DeletaPokemonPorId(int Id)
+		{
+			Console.WriteLine("Deletando Pokemon de Id {0}", Id);
+			PokemonsCadastrados.Remove(PokemonsCadastrados.Find(P => P.Codigo == Id));
+		}
+	}
+}
---
+++ Atividade API REST/Atividade API REST/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade_API_REST.Models
+{
+	public class Pokemon
+	{
+		public int Codigo { get; set; }
+		public string Nome { get; set; }
+		public string Tipo { get; set; }
+	}
+}
