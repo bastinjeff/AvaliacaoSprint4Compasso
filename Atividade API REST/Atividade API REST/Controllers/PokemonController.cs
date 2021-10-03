8bc2af5804c0918fb86a646b00417b5276ca34ae
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-03 19:26:24 -0300

    Refatoração e Adição de comentários
    Ocorreu uma pequena refatoração de código, mas mais importante foi a adição de comentários de para exemplificar rotas de GET, POST e DELETE do código

--- Atividade API REST/Atividade API REST/Controllers/PokemonController.cs
+++ Atividade API REST/Atividade API REST/Controllers/PokemonController.cs
@@ -14,6 +14,7 @@ namespace Atividade_API_REST.Controllers
 		static List<Pokemon> PokemonsCadastrados = new List<Pokemon>();
 		static int ContadorHipotetico = 0;
 
+		//URL EXEMPLO(POST, Necessario Corpo JSON): localhost:44302/api/Pokemon 
 		[HttpPost]
 		public IActionResult AdicionaPokemon([FromBody] Pokemon pokemon)
 		{
@@ -24,6 +25,7 @@ namespace Atividade_API_REST.Controllers
 			return CreatedAtAction(nameof(RecuperaPokemonPorId), new { Id = pokemon.Codigo }, pokemon);
 		}
 
+		//URL EXEMPLO (GET): localhost:44302/api/Pokemon
 		[HttpGet]
 		public IActionResult RecuperaTodosOsPokemons()
 		{
@@ -31,6 +33,7 @@ namespace Atividade_API_REST.Controllers
 			return Ok(PokemonsCadastrados);
 		}
 
+		//URL EXEMPLO (GET): localhost:44302/api/Pokemon/1
 		[HttpGet("{id}")]
 		public IActionResult RecuperaPokemonPorId(int Id)
 		{
@@ -43,6 +46,7 @@ namespace Atividade_API_REST.Controllers
 			return Ok(Retornado);
 		}
 
+		//URL EXEMPLO(DELETE): localhost:44302/api/Pokemon/1
 		[HttpDelete("{id}")]
 		public IActionResult DeletaPokemonPorId(int Id)
 		{
